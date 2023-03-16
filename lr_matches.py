eeddcc6c24c9a687cbc1c5c7546bc3d833101983
adambuttrick <adambuttrick@gmail.com>
2023-03-15 22:49:08 -0700

    Adding requirements.txt, removing specific APS reference, adding comments, removing unnecessary variable

--- lr_matches.py
+++ lr_matches.py
@@ -10,11 +10,13 @@ from urllib.parse import quote
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 
-
+# Remove country references used in the primary names of some ROR records for disambiguation
 def preprocess_primary_name(name):
     name = re.sub(r'\s\(.*\)', '', name)
     return name
+    
 
+# Used to skip labels that have non-Latin characters (and thus cannot be used in pseudo affiliation strings)
 def check_latin_chars(s):
     for ch in s:
         if ch.isalpha():
@@ -22,6 +24,9 @@ def check_latin_chars(s):
                 return False
     return True
 
+
+# Create pseduo affiliation strings from the names and locations in the matches for use
+# by the classifier
 def format_matches(results):
     matches = []
     records = [result['organization'] for result in results['items']]
@@ -30,16 +35,22 @@ def format_matches(results):
         primary_name = preprocess_primary_name(record['name'])
         aliases, labels, acronyms = record['aliases'], record['labels'], record['acronyms']
         if labels != []:
-            labels = [label['label'] for label in labels if check_latin_chars(label['label'])]
-        variants =  aliases + labels + acronyms
+            labels = [label['label']
+                      for label in labels if check_latin_chars(label['label'])]
+        variants = aliases + labels + acronyms
         address = record['addresses'][0]
-        city_name = address['geonames_city']['city'] if 'city' in address['geonames_city'].keys() else None
-        admin1_name = address['geonames_city']['geonames_admin1']['name'] if 'geonames_admin1' in address['geonames_city'].keys() else None
+        city_name = address['geonames_city']['city'] if 'city' in address['geonames_city'].keys(
+        ) else None
+        admin1_name = address['geonames_city']['geonames_admin1']['name'] if 'geonames_admin1' in address['geonames_city'].keys(
+        ) else None
         country_name = record['country']['country_name']
-        location = ' '.join([part for part in [city_name, admin1_name, country_name] if part != None])
-        record_metadata = {'ror_id': ror_id, 'primary_name':primary_name, 'variant_names':variants, 'location':location}
+        location = ' '.join(
+            [part for part in [city_name, admin1_name, country_name] if part != None])
+        record_metadata = {'ror_id': ror_id, 'primary_name': primary_name,
+                           'variant_names': variants, 'location': location}
         matches.append(record_metadata)
     return matches
+
 
 def normalize_text(text, ws_flag=False):
     text = unidecode(text.lower())
@@ -50,9 +61,11 @@ def normalize_text(text, ws_flag=False):
     text = ''.join(ch for ch in text if ch not in exclude)
     return text
 
+
 def predict_ror_id_weighted(affiliation, matches):
     # Preprocess the affiliation matches
-    text_data = [normalize_text(affiliation, True)] + [normalize_text(m["primary_name"] + " " + " ".join(m["variant_names"]) + " " + m["location"]) for m in matches]
+    text_data = [normalize_text(affiliation, True)] + [normalize_text(
+        m["primary_name"] + " " + " ".join(m["variant_names"]) + " " + m["location"]) for m in matches]
     print(text_data)
     # Extract TF-IDF features from the affiliation matches
     tfidf = TfidfVectorizer(stop_words="english")
@@ -63,8 +76,8 @@ def predict_ror_id_weighted(affiliation, matches):
     match_features = tfidf_features[1:]
 
     # Take into account the distribution of probable matches returned by the affiliation service
-    # (80% likehood of a match in the first five results, with an incremental drop off subsequently), 
-    # then apply the match weights based on the index of the match in the match set. 
+    # (80% likehood of a match in the first five results, with an incremental drop off subsequently),
+    # then apply the match weights based on the index of the match in the match set.
     num_matches = len(matches)
     if num_matches <= 5:
         match_weights = [0.8 / (i + 1) for i in range(num_matches)]
@@ -72,7 +85,7 @@ def predict_ror_id_weighted(affiliation, matches):
         match_weights = [0.8 / (i + 1) for i in range(5)]
         for i in range(5, num_matches):
             weight = 0.8 / (5 + 2 * (i - 5))
-            match_weights.append(weight)            
+            match_weights.append(weight)
     match_weights = np.array(match_weights).reshape(-1, 1)
 
     # Compute the weighted match features
@@ -87,6 +100,7 @@ def predict_ror_id_weighted(affiliation, matches):
 
     return predicted_ror_id
 
+
 def get_best_match(affiliation_string, results):
     num_results = results['number_of_results']
     if num_results == 1:
@@ -97,10 +111,11 @@ def get_best_match(affiliation_string, results):
         return prediction
     return 'No results'
 
+
 def query_affiliation(affiliation, ror_id):
     try:
         affiliation_encoded = quote(affiliation)
-        url = f"http://localhost:9292/organizations?affiliation={affiliation_encoded}"
+        url = f"https://api.ror.org/organizations?affiliation={affiliation_encoded}"
         response = requests.get(url)
         results = response.json()
         best_match_prediction = get_best_match(affiliation, results)
@@ -124,25 +139,28 @@ def query_affiliation(affiliation, ror_id):
                 ror_id_chosen = True
         return url, ror_id_in_results, ror_id_chosen, index, chosen_id
     except Exception:
-        return '','','','','Error'
+        return '', '', '', '', 'Error'
 
 
 def parse_and_query(f):
-    outfile = 'aps_test_results_first_five.csv'
+    outfile = 'test_results.csv'
     with open(outfile, 'w') as f_out:
         writer = csv.writer(f_out)
-        writer.writerow(['aps_ror_id','doi','aps_institution','affiliation', 'query_url','in_results', 'chosen','index', 'chosen_id'])
+        #adjust header to match your input file
+        writer.writerow(['ror_id', 'doi', 'institution', 'affiliation',
+                         'query_url', 'in_results', 'chosen', 'index', 'chosen_id'])
     with open(f, 'r+', encoding='utf-8-sig') as f_in:
         reader = csv.DictReader(f_in)
         for row in reader:
             affiliation = row['affiliation']
-            aps_ror_id = row['aps_ror_id']
-            matches_aff_ror_id = False
-            if aps_ror_id != '' and aps_ror_id != None:
-                query_url, ror_id_in_results, ror_id_chosen, index, chosen_id = query_affiliation(affiliation, aps_ror_id)
+            assigned_ror_id = row['ror_id']
+            if assigned_ror_id != '' and assigned_ror_id != None:
+                query_url, ror_id_in_results, ror_id_chosen, index, chosen_id = query_affiliation(
+                    affiliation, assigned_ror_id)
                 with open(outfile, 'a') as f_out:
                     writer = csv.writer(f_out)
-                    writer.writerow(list(row.values()) + [query_url, ror_id_in_results, ror_id_chosen, index, chosen_id])
+                    writer.writerow(list(
+                        row.values()) + [query_url, ror_id_in_results, ror_id_chosen, index, chosen_id])
 
 
 if __name__ == '__main__':
